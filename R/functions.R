a696c9163690f79c482d431cedf35ab8550f6fd2
Thomas Faria <thomas.faria@ensae.fr>
2023-02-10 17:04:57 +0100

    add functions

---
+++ R/functions.R
@@ -0,0 +1,71 @@
+get_ECB_vintage <- function () {
+  current_date <- Sys.Date()
+  month <- lubridate::month(current_date)
+  year <- as.character(lubridate::year(current_date))
+  if (month == 2) {
+    vintage <- paste0("W", substr(year, 3, 4))
+  } else  if (month == 5){
+    vintage <- paste0("G", substr(year, 3, 4))
+  }else  if (month == 8){
+    vintage <- paste0("S", substr(year, 3, 4))
+  }else  if (month == 11){
+    vintage <- paste0("A", substr(year, 3, 4))
+  }
+  return(vintage)
+}
+
+get_dimensions <- function(dataset, data_infos, environement) {
+  sublist <- Filter(function(x) x$Database == dataset, data_infos)
+  
+  splitted_codes <- lapply(sublist, function(x) {
+    splitted_code <- strsplit(x$Series_code, ".", fixed = TRUE)[[1]]
+  })
+  splitted_codes<-as.matrix(do.call(cbind, splitted_codes))
+  
+  dimensions <- split(as.vector(splitted_codes), row(splitted_codes))
+  names(dimensions) <- environment$Data_structure[[dataset]]
+  dimensions$REF_AREA <- environment$Countries
+  codes <- as.vector(sapply(sublist, function(x) {splitted_code <- x$Series_code}))
+  
+  return(list("Dim" = dimensions, "Codes" = codes))
+  
+}
+
+get_ECB_data <- function(dataset, data_infos, environement) {
+  
+  dimensions <- get_dimensions(dataset, data_infos, environement)
+  
+  data<-rdbnomics::rdb(provider_code = "ECB",
+                       dataset_code = dataset,
+                       dimensions = dimensions$Dim)[
+                         , .(dataset_code, series_code, indexed_at, period, STO, 
+                             `Stocks, Transactions, Other Flows`,
+                             `Reference area`, REF_AREA, value)
+                       ][grepl(paste0(dimensions$Codes, collapse = "|"), series_code)]
+  
+  data.table::setnames(data, c("dataset_code", "series_code", "indexed_at", "period", "REF_AREA", "Reference area", "value"), 
+                       c("Database", "Series_code", "Snapshot_date", "Date", "Country_code", "Country_long", "Value"),
+                       skip_absent = TRUE
+  )
+  
+  sublist <- Filter(function(x) x$Database == dataset, data_infos)
+  
+  join_dt <- data.table::data.table(do.call(rbind, lapply(sublist, data.frame, stringsAsFactors = FALSE)))
+  
+  data = merge(data, join_dt[, .(STO, Variable_code, Variable_long)], by = "STO")
+  
+  vintage <- get_ECB_vintage()
+  data[, ECB_vintage := vintage]
+  return(data[, .(Database, Series_code, Snapshot_date, ECB_vintage, Country_code, Country_long, 
+                  STO, `Stocks, Transactions, Other Flows`, Variable_code, Variable_long, Date, Value)])
+}
+
+get_RTDB <- function(file) {
+  data <- data.table::data.table(arrow::read_csv_arrow(file))
+  return(data)
+}
+
+append_dataset <- function(list_data) {
+  new_db <- data.table::rbindlist(list_data, use.names=TRUE)
+  return(new_db)
+}
