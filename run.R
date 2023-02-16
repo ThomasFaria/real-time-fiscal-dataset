f753a46b58ec68c0e34da161192e821ee4253c01
Thomas Faria <thomas.faria@insee.fr>
2023-02-16 18:45:09 +0100

    create 2 specific targets

--- run.R
+++ run.R
@@ -1,10 +1,10 @@
-#!/usr/bin/env Rscript
-
-# This is a helper script to run the pipeline.
-# Choose how to execute the pipeline below.
-# See https://books.ropensci.org/targets/hpc.html
-# to learn about your options.
-
+Sys.setenv(TAR_PROJECT = "GFS")
 targets::tar_make()
-# targets::tar_make_clustermq(workers = 2) # nolint
-# targets::tar_make_future(workers = 2) # nolint
+
+Sys.setenv(TAR_PROJECT = "MNA")
+targets::tar_make()
+
+
+arrow::write_csv_arrow(real_time_fiscal_database |>
+                         dplyr::rename(Name = "Stocks, Transactions, Other Flows"),
+                       "realtime_fiscal_database.csv")
